525c178b2e24c4b16cbb35227bb402eabf84a5ca
Mark <markjohnson@bath.edu>
2019-05-29 00:11:11 +0100

    Updates

---
+++ precache-manifest.2548c4229a07bf56ac68075193ba0582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "540826bcd6dc8644e1f7",
+    "url": "/static/css/main.d4a3e34a.chunk.css"
+  },
+  {
+    "revision": "540826bcd6dc8644e1f7",
+    "url": "/static/js/main.b5b5d6be.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b4e9052f1239cdf7bcc0",
+    "url": "/static/js/2.64b46f6c.chunk.js"
+  },
+  {
+    "revision": "36b44c9b710c6e01e399f10b35eac7d0",
+    "url": "/index.html"
+  }
+];
